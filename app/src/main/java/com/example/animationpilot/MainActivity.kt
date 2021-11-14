7480e3d0f80f76806899927e8042df45c39c1b6f
Seolang <dntmdqls0912@gmail.com>
2021-11-15 08:49:43 +0900

    Divide Shake alpha value of function

--- app/src/main/java/com/example/animationpilot/MainActivity.kt
+++ app/src/main/java/com/example/animationpilot/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.animationpilot.effects.Dissolve
-import com.example.animationpilot.effects.Fade
-import com.example.animationpilot.effects.Shake
+import androidx.compose.ui.unit.dp
+import com.example.animationpilot.effects.*
 import com.example.animationpilot.ui.theme.AnimationPilotTheme
 
 
@@ -26,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     Column {
                         Dissolve()
                         Fade()
-                        Shake()
+                        Shake1()
                     }
                 }
             }
--- app/src/main/java/com/example/animationpilot/effects/Animation.kt
+++ app/src/main/java/com/example/animationpilot/effects/Animation.kt
@@ -1,5 +1,6 @@
 package com.example.animationpilot.effects
 
+import android.widget.Toast
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
@@ -11,7 +12,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.motion.widget.KeyFrames
 import com.example.animationpilot.enum.MyImages
@@ -100,20 +103,21 @@ fun Fade() {
 @ExperimentalAnimationApi
 @Composable
 fun Shake() {
+    val repTime = 4
     var isShake by rememberSaveable { mutableStateOf(false) }
     val alpha by animateDpAsState(
         // if button clicked, isShake's state is changed so target .dp also changed
         targetValue = if (isShake) 12.dp else 0.dp,
         animationSpec = keyframes {
             durationMillis = 100
-            (-12).dp.at(durationMillis/4)
-            (12).dp.at(durationMillis/2)
-            (-12).dp.at(durationMillis*3/4)
-            (12).dp.at(durationMillis)
+            for(item in 1..repTime/2) {
+                (-12).dp.at(durationMillis*(2*item-1) / repTime)
+                (12).dp.at(durationMillis*(2*item) / repTime)
+            }
         }
     // after finish half of animation( which means dp changed 0 to 12 )
     // change isShake state to default so animation start again from 12 to 0
-    ) { run { if(isShake) isShake = false } }
+    ) { if(isShake) isShake = false }
 
     Column(
         modifier = Modifier.padding(10.dp),
@@ -131,6 +135,67 @@ fun Shake() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .offset(x = alpha)
+            )
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun ShakePosition(trigger: Boolean, moveWidth: Dp, durationTime: Int, repeat: Int,
+                  finishedListener: ((Dp) -> Unit)? = null): State<Dp> {
+    // to change alpha along with isShake, isShake must saved in composition, by remember
+    val cxt = LocalContext.current
+    val alpha = animateDpAsState(
+        targetValue = if (trigger) moveWidth else 0.dp,
+        animationSpec = keyframes {
+            durationMillis = durationTime
+            for(item in 1..repeat) {
+                (-moveWidth).at(durationMillis*(2*item-1) / repeat)
+                moveWidth.at(durationMillis*(2*item) / repeat)
+            }
+        },
+        finishedListener
+    )
+
+    return alpha
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun Shake1() {
+    var isShake by remember { mutableStateOf(false) }
+    val cxt = LocalContext.current
+    val alpha by ShakePosition(
+        trigger = isShake,
+        moveWidth = 12.dp,
+        durationTime = 1000,
+        repeat = 4
+    ) {
+        isShake = false
+        Toast.makeText(cxt, "핸들러가 작동했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    Column(
+        modifier = Modifier.padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+
+
+        Button(
+            // Click image change state
+            onClick = {
+                //Toast.makeText(cxt, "지금 isShake 는 $isShake 입니다.", Toast.LENGTH_SHORT).show()
+                if(!isShake) isShake = true
+                      },
+        ) {
+            Image(
+                painter = painterResource(id = MyImages.Android.image),
+                contentDescription = MyImages.Android.name,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(x = if (isShake) alpha else 0.dp)
             )
         }
     }
